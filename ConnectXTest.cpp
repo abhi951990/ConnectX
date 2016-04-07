cc09f2695c9af3d0df43f2b3b3bdce06edb6a25a
Abhinav Gupta <abhi.951990@gmail.com>
2016-04-07 15:44:50 -0700

    Found first bug

--- ConnectXTest.cpp
+++ ConnectXTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include "ConnectX.h"
- 
+
 class ConnectXTest : public ::testing::Test
 {
 	protected:
@@ -18,4 +18,76 @@ class ConnectXTest : public ::testing::Test
 TEST(ConnectXTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
+}
+
+TEST(ConnectXTest,placePieceInvalid){
+  ConnectX conn;
+  // bool flag=false;
+
+  ASSERT_EQ(-1,conn.at(2,6));
+  // ASSERT_EQ(0,conn.at(0,0));
+  ASSERT_EQ(-1,conn.at(-1,0));
+  ASSERT_EQ(-1,conn.at(-1,1));
+  ASSERT_EQ(-1,conn.at(-1,2));
+
+
+  // conn.placePiece(2);
+  // ASSERT_TRUE(flag);
+}
+
+TEST(ConnectXTest,placePiece){
+  ConnectX conn;
+  // bool flag=false;
+  conn.placePiece(2);
+  conn.placePiece(3);
+  conn.placePiece(3);
+  conn.placePiece(2);
+  conn.placePiece(3);
+  conn.placePiece(3);
+  conn.placePiece(2);
+  conn.placePiece(3);
+  conn.placePiece(2);
+  conn.placePiece(2);
+  conn.placePiece(2);
+  conn.placePiece(2);
+  conn.placePiece(3);
+  conn.placePiece(3);
+  conn.placePiece(1);
+  conn.placePiece(4);
+  conn.placePiece(1);
+  conn.placePiece(1);
+  conn.placePiece(4);
+  conn.placePiece(1);
+  conn.placePiece(1);
+  conn.placePiece(1);
+  conn.placePiece(4);
+  conn.placePiece(1);
+  conn.placePiece(4);
+  conn.placePiece(4);
+  conn.placePiece(0);
+  conn.placePiece(5);
+  conn.placePiece(5);
+  conn.placePiece(5);
+  conn.placePiece(5);
+  conn.placePiece(5);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  conn.showBoard();
+  ASSERT_EQ(2,conn.at(2,5));
+
+  // ASSERT_TRUE(flag);
+
+
 }
---
+++ README.md
@@ -0,0 +1,7 @@
+Abhinav Gupta
+
+Quiz 3 Bug Report
+
+* Unexpected output at (-1,0) position on Board after creating object of ConnectX class.
+	* Expected value at (-1,0) after creating object = -1
+	* Actual value at (-1,0) after creating object= 0
